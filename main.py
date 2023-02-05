efc95b83189fa1bb8e0e250e9cc5099246866f1a
VJBots <122880036+VJBots@users.noreply.github.com>
2023-02-05 20:38:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def echo(client: pyrogram.client.Client, message: pyrogram.types.messages_and_me
         return
 
     app.send_message(message.chat.id, '**Hi, I am Mdisk Video Downloader, you can watch Videos without MX Player.\n__Send me a link to Start...__**',reply_to_message_id=message.id,
-    reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("📦 Channel ", url="https://t.me/WebXBots")]]))
+    reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("📦 Channel ", url="https://t.me/VJ_Bots")]]))
 
 # help command
 @app.on_message(filters.command(["help"]))
